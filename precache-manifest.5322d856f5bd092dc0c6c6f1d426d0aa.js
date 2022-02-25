2cec33351ad80713e1c37d44cd725691a3b320e3
Eternal-could <20578466@qq.com>
2022-02-25 14:20:54 +0800

    deploy

---
+++ precache-manifest.5322d856f5bd092dc0c6c6f1d426d0aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3567c48d61ed42d859b3",
+    "url": "/bill-view-website/css/app.487eb477.css"
+  },
+  {
+    "revision": "08fa833896e9042e462b14f1fd6ec037",
+    "url": "/bill-view-website/index.html"
+  },
+  {
+    "revision": "3567c48d61ed42d859b3",
+    "url": "/bill-view-website/js/app.cd84f448.js"
+  },
+  {
+    "revision": "aedde47bcd9fa6421071",
+    "url": "/bill-view-website/js/chunk-vendors.19a25895.js"
+  },
+  {
+    "revision": "1f134956c66d295d990604debf55d6fe",
+    "url": "/bill-view-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bill-view-website/robots.txt"
+  }
+]);
